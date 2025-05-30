b81f53b9b2f61994abc98a19f3a5ad1a7d659025
DylanCTT <clattdylan@gmail.com>
2025-05-30 00:32:49 -0300

    arranque practica 5

---
+++ Practica/AYED/src/tp5Ej1/Edge.java
@@ -0,0 +1,11 @@
+package tp5Ej1;
+
+public interface Edge<T> {
+	
+	public Vertex<T> target();
+	
+	public int getWeight();
+
+
+
+}
---
+++ Practica/AYED/src/tp5Ej1/Vertex.java
@@ -0,0 +1,12 @@
+package tp5Ej1;
+
+public interface Vertex<T> {
+	
+	public T getData();
+	
+	public void setData(T data);
+	
+	public int getPosition();
+
+
+}
