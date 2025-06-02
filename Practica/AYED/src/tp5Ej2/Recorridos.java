b556132426a44708758df953e66aeaadcad0e5d1
DylanCTT <clattdylan@gmail.com>
2025-06-02 02:32:57 -0300

    practica 5

--- Practica/AYED/src/tp5Ej2/Recorridos.java
+++ Practica/AYED/src/tp5Ej2/Recorridos.java
@@ -1,65 +1,76 @@
 package tp5Ej2;
 
+import java.util.LinkedList;
 import java.util.List;
 
-import tp1Ej8.Queue;
+import tp1Ej8.DoubleEndedQueue;
+import tp5Ej1.AdjListGraph;
+import tp5Ej1.AdjListVertex;
 import tp5Ej1.Edge;
-import tp5Ej1.Graph;
 import tp5Ej1.Vertex;
 
 public class Recorridos<T> {
 
 	
-	public void dfs(Graph<T> grafo) {
-		boolean [] marca =new boolean [grafo.getSize()];  //en este vector guardo las visitas de los vertices
-		for (int i=0; i<grafo.getSize(); i++) {
-			if (!marca[i]) {
-				System.out.println("larco con: "+ grafo.getVertex(i).getData());
-				dfs (i,grafo,marca);
-			}
-		}
-	}
-	
-	private void dfs(int i, Graph<T> grafo, boolean[] marca) {
-		marca[i] = true;
-		Vertex<T> v = grafo.getVertex(i);
-		System.out.println(v);
-		List<Edge<T>> adyacentes = grafo.getEdges(v); //declaro lista de adyacentes de el vertice
-		for (Edge<T> e: adyacentes){  //para cada adyacente guardo en lista
-			int j = e.getTarget().getPosition();
-			if (!marca[j]) {  //cheque si ya pase por el vertice para agregarlo
-				dfs(j, grafo, marca);
-			}
-		}
-	}
-	
-	public void bfs(Graph<T> grafo) {
-		 boolean[] marca = new boolean[grafo.getSize()];
-		 for (int i = 0; i <= marca.length; i++) {
-				 if (!marca[i]){
-						 this.bfs(i, grafo, marca); 
-				 }
-		 }
-	}
-	
-	private void bfs(int i, Graph<T> grafo, boolean[] marca) {
-		Queue<Vertex<T>> q = new Queue<Vertex<T>>();
-		q.enqueue(grafo.getVertex(i));
-		marca[i] = true;
-		while (!q.isEmpty()) {
-				Vertex<T> w = q.dequeue();
-				System.out.println(w);
-				// para todos los vecinos de w:
-				List<Edge<T>> adyacentes = grafo.getEdges(w);
-				for (Edge<T> e: adyacentes) {
-						int j = e.getTarget().getPosition();
-						if (!marca[j]) {
-								marca[j] = true;
-								//Vertex<T> v = e.getTarget();
-								q.enqueue(e.getTarget());
-						}
+	public List<T> dfs(AdjListGraph<T> grafo) {
+		if(grafo!=null && !grafo.isEmpty()) {
+			boolean [] visitados =new boolean [grafo.getSize()];  //en este vector guardo las visitas de los vertices
+			LinkedList<T> aux=new LinkedList<T>();
+			int i;
+			for (i=0; i<grafo.getSize(); i++) {
+				if (!visitados[i]) {
+					System.out.println("larco con: "+ grafo.getVertex(i).getData());
+					dfs((AdjListVertex<T>) grafo.getVertices().get(i),visitados, aux);
 				}
+			}
+			return aux;
+		}
+		return null;
+	}
+	
+	private void dfs(AdjListVertex<T> V, boolean[] visitados ,List<T> listD) {
+		visitados[V.getPosition()] = true;
+		listD.add(V.getData());
+		
+		List<Edge<T>> adyacentes = V.getEdges(); //declaro lista de adyacentes de el vertice
+		for (Edge<T> e: adyacentes){  //para cada adyacente guardo en lista
+			if (!visitados[e.getTarget().getPosition()]) {  //cheque si ya pase por el vertice para agregarlo
+				dfs((AdjListVertex<T>) e.getTarget(), visitados, listD);
+			}
 		}
 	}
+	
+	public List<T> Bfs(AdjListGraph<T> grafo){
+		if(grafo!=null&&!grafo.isEmpty()) {
+			DoubleEndedQueue<Vertex<T>> cola = new DoubleEndedQueue<Vertex<T>>();
+			List<Vertex<T>> listaV = grafo.getVertices();
+			List<Edge<T>> listaE;
+			boolean visitados[]= new boolean[grafo.getSize()];
+			List<T> listaR = new LinkedList<T>();
+		
+			Vertex<T> act;
+			for(int j=0;j<listaV.size();j++) {
+				if(!visitados[listaV.get(j).getPosition()]) {
+					cola.enqueue(listaV.get(j));
+					while(!cola.isEmpty()) {
+						act = cola.dequeue();
+						listaR.add(act.getData());
+						listaE = grafo.getEdges(act);
+						for(Edge<T> e : listaE) {
+							if(!visitados[e.getTarget().getPosition()]) {
+								cola.enqueue(e.getTarget());
+								visitados[e.getTarget().getPosition()]=true;
+							}
+						}
+					}
+				}
+			}
+			return listaR;
+		}
+		return null;
+	}
+	
+	
+	
 
 }
---
+++ Practica/AYED/src/tp5Ej3/Mapa.java
@@ -0,0 +1,24 @@
+package tp5Ej3;
+
+import java.util.List;
+
+import tp5Ej1.AdjListGraph;
+import tp5Ej1.Graph;
+
+public class Mapa {
+	
+	private AdjListGraph<String> mapaCiudades;
+
+	public void setMapaCiudades(AdjListGraph<String> mapaCiudades) {
+		this.mapaCiudades = mapaCiudades;
+	}
+
+	public List<String> devolverCamino (String ciudad1, String ciudad2){
+		if (mapaCiudades!=null && !mapaCiudades.isEmpty())
+		
+	}
+	
+	
+	
+
+}
